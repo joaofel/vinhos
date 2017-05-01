a63c89d2400d68143b80d04acc8d94b9e855fa88
João Feliciano <joao.hfj@gmail.com>
2017-04-30 22:28:49 -0300

    Adicionado seguranca com o Spring Security. Finalizado o Grud. Adicionado tela de login. Adicionado tela de erro 403.

--- aw-vinhos/src/main/java/com/joaofeliciano/vinhos/repository/Vinhos.java
+++ aw-vinhos/src/main/java/com/joaofeliciano/vinhos/repository/Vinhos.java
@@ -1,9 +1,13 @@
 package com.joaofeliciano.vinhos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.joaofeliciano.vinhos.model.Vinho;
 
 public interface Vinhos extends JpaRepository<Vinho, Long> {
-
+	
+	public List<Vinho> findByNomeContainingIgnoreCase(String nome);
+	
 }
